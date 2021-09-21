c0d130186fc19dcd32b75a7a4154f1aaa1cdf56a
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2021-09-21 16:04:15 -0400

    ContentList component created.

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  { ContentList } from "../helper-files/content-list";
+// import  { ContentList } from "../helper-files/content-list";
 import { Content } from "../helper-files/content-interface";
 
 @Component({
@@ -10,18 +10,20 @@ import { Content } from "../helper-files/content-interface";
 export class ContentCardComponent implements OnInit {
 
   //instance of your ContentList
-  anotherContent: ContentList = new ContentList;
-  constructor() { }
+  // anotherContent: ContentList = new ContentList;
+  constructor() {
+
+  }
 
   ngOnInit(): void {
 
     let item: Content;
 
-    item = {id: 1, author: "Unknown", title: "The Novel", body: "This is the novel."};
-    this.anotherContent.add(item);
-    item = {id: 2, author: "Unknown", title: "Movie", body: "This is the movie."};
-    this.anotherContent.add(item);
-    item = {id: 3, author: "Unknown", title: "The cartoon", body: "This is the cartoon."};
-    this.anotherContent.add(item);
+    // item = {id: 1, author: "Unknown", title: "The Novel", body: "This is the novel."};
+    // this.anotherContent.add(item);
+    // item = {id: 2, author: "Unknown", title: "Movie", body: "This is the movie."};
+    // this.anotherContent.add(item);
+    // item = {id: 3, author: "Unknown", title: "The cartoon", body: "This is the cartoon."};
+    // this.anotherContent.add(item);
   }
 }
---
+++ src/app/content-list/content-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.css']
+})
+export class ContentListComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //add 5 items in array
+
+  }
+
+}
